05e4bdace795019efaa01ba8f079b0efa4e2e8e0
Haniyeh Z. Budaqi <haniyehz.budaqi@gmail.com>
2026-02-19 20:14:36 +0330

    Added concept check 1 for module 3

---
+++ Control of Nonlinear Spacecraft Attitude Motion/Module 3/Module3.m
@@ -0,0 +1,139 @@
+%% Concept Check 1 - General 3-Axis Attitude Control
+
+% Question 3
+I = diag([100;75;80]); % Intertia in kg.m^2
+
+sigma_BN_0 = [0.1;0.2;-0.1]; % Initial MRP
+omega_BN_0 = [30;10;-20] * pi/180; % Initial Angular Velocity
+
+K = 5; % N.m
+P = diag([10;10;10]);
+L = 0; % External torque
+
+% With the reference frame being the same as the inertial frame we have
+% [I]w_dot = -K.sigma - [P]w
+dt = 0.1;
+t = 0:dt:120;
+sigma_i = sigma_BN_0;
+omega_i = omega_BN_0;
+
+normSigma30 = 0;
+
+for i = t
+    sigma_dot = (1/4)*B_sigma(sigma_i)*omega_i;
+    omega_dot = I\(-K*sigma_i - P*omega_i);
+    
+    sigma_i = sigma_i + sigma_dot*dt;
+    if (norm(sigma_i) > 1)
+       sigma_i = sigmaToSigmaS(sigma_i); 
+    end
+    
+    omega_i = omega_i + omega_dot*dt;
+    
+    if(i == 30) 
+        normSigma30 = norm(sigma_i);
+    end
+end
+
+% Question 4
+sigma_BN_0 = [0.1;0.2;-0.1];
+w_BN_0 = [30;10;-20] * pi/180;
+f = 0.05;
+
+tf = 43;
+dt = 0.1;
+
+t = 0:dt:tf;
+sigma_BN_i = sigma_BN_0;
+w_BN_i = w_BN_0;
+sigma_BR_i = 0;
+K = 5;
+P = 10*eye(3);
+L = 0;
+I = diag([100;75;80]);
+
+k40 = round(40/dt) + 1;
+k = 0;
+w_RN_prev = [];     % in R-frame
+
+for ti = t
+  k = k + 1;
+
+  % Reference sigma and sigma_dot
+  sigma_RN_i = [0.2*sin(f*ti); 0.3*cos(f*ti); -0.3*sin(f*ti)];
+  sigma_dot_RN_i = f*[0.2*cos(f*ti); -0.3*sin(f*ti); -0.3*cos(f*ti)];
+
+  sigma_RN_i_1 = [0.2*sin(f*(ti+dt)); 0.3*cos(f*(ti+dt)); -0.3*sin(f*(ti+dt))];
+  sigma_dot_RN_i_1 = f*[0.2*cos(f*(ti+dt)); -0.3*sin(f*(ti+dt)); -0.3*cos(f*(ti+dt))];
+
+  % omega_R/N in R frame
+  w_RN_i   = 4 * (B(sigma_RN_i)   \ sigma_dot_RN_i);
+  w_RN_i_1 = 4 * (B(sigma_RN_i_1) \ sigma_dot_RN_i_1);
+  w_dot_RN_i = (w_RN_i_1 - w_RN_i) / dt;   % in R frame
+
+  % Attitude error sigma_B/R
+  sigma_BR_i = mrpAddition(sigma_BN_i, -sigma_RN_i);
+
+  % Shadow set on sigma_BR (recommended)
+  if norm(sigma_BR_i) > 1
+      sigma_BR_i = sShadow(sigma_BR_i);
+  end
+
+  % DCM C_B/R mapping R->B
+  C_BR = mrp2dcm(sigma_BR_i);
+
+  % Rotate reference rates into B frame
+  w_RN_B     = C_BR * w_RN_i;
+  
+  % Rate error in B
+  delta_w_i = w_BN_i - w_RN_B;
+  
+  w_dot_RN_B = C_BR * w_dot_RN_i + cross(delta_w_i, w_RN_B);
+
+  % Control (all in B frame now)
+  u = -P*delta_w_i - K*sigma_BR_i ...
+      + I*(w_dot_RN_B - cross(w_BN_i, w_RN_B)) ...
+      + cross(w_BN_i, I*w_BN_i) - L;
+
+  % Save norm at 40 s
+  if k == k40
+    sigmaNorm40 = norm(sigma_BR_i);
+  end
+
+  % Plant propagation
+  sigma_dot_BN_i = 0.25 * B(sigma_BN_i) * w_BN_i;
+  w_dot_BN_i = I \ (-cross(w_BN_i, I*w_BN_i) + u + L);
+
+  sigma_BN_i = sigma_BN_i + sigma_dot_BN_i * dt;
+  w_BN_i     = w_BN_i     + w_dot_BN_i     * dt;
+
+  % Shadow set on sigma_BN
+  if norm(sigma_BN_i) > 1
+      sigma_BN_i = sShadow(sigma_BN_i);
+  end
+end
+
+
+fprintf('Norm of sigma at t = 40s is : %.6f\n', sigmaNorm40);
+
+function s12 = mrpAddition(s1, s2)
+  s1_2 = dot(s1,s1);
+  s2_2 = dot(s2,s2);
+  num = (1 - s2_2)*s1 + (1 - s1_2)*s2 - 2*cross(s1, s2);
+  den = 1 + s1_2*s2_2 - 2*dot(s1, s2);
+  s12 = num / den;
+end
+
+function B_sigma = B(sigma)
+  B_sigma = (1 - norm(sigma)^2) * eye(3) + 2*tilda(sigma) + 2*sigma*(sigma.');
+end
+
+function shadow = sShadow(sigma)
+  shadow = -sigma / (norm(sigma)^2);
+end
+
+function C = mrp2dcm(s)
+  s2 = dot(s,s);
+  S  = tilda(s);
+  C  = eye(3) + (8*S*S - 4*(1 - s2)*S) / (1 + s2)^2;
+end
